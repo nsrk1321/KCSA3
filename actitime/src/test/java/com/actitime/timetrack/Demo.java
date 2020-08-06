09b7851b190cc2e0a43a40956e12a689aea03a6a
NSRK <NSRK@DESKTOP-4SSU5PL>
2020-08-06 18:21:25 +0530

    project commit

---
+++ actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,14 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+@Test
+	public void run()
+	{
+		Reporter.log("run successufully", true);
+		Reporter.log("run successufully", true);
+		Reporter.log("run successufully", true);
+	}
+}
