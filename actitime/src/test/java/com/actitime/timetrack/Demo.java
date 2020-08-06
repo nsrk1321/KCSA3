bb7e575ad33b4418b4a717220a8db933612275cc
NSRK <NSRK@DESKTOP-4SSU5PL>
2020-08-06 18:36:23 +0530

    commit

--- actitime/src/test/java/com/actitime/timetrack/Demo.java
+++ actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -7,8 +7,11 @@ public class Demo {
 @Test
 	public void run()
 	{
-		Reporter.log("run successufully", true);
-		Reporter.log("run successufully", true);
-		Reporter.log("run successufully", true);
+		
+	Reporter.log("run successufully", true);
+		
+		//i am krishna have seen my code
+	
+	//skjfoeijojslkas
 	}
 }
