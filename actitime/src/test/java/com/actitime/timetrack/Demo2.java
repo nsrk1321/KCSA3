1406d295cb1b420edfb0d976363bde8464ff8801
NSRK <NSRK@DESKTOP-4SSU5PL>
2020-08-08 11:06:35 +0530

    commited

---
+++ actitime/src/test/java/com/actitime/timetrack/Demo2.java
@@ -0,0 +1,15 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2 {
+@Test
+	public void run()
+	{
+		
+	Reporter.log("run successufully demo 2", true);
+	Reporter.log("run successufully continuously ", true);
+	
+	}
+}
